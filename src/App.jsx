92ad18924139ef85f2cc6c9c40779be130c40a31
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-07-05 17:05:15 +0200

    Implement app layout
    - Implemented the first layout of the app.
    
     This includes:
    1. Sidebar window on the left
    2. Searchbar window on top of the sidebar window
    3. Chat window on the right
    4. Recipient window on top of the chat window

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+import ChatWindow from './ChatWindow.jsx'
+import Sidebar from './Sidebar.jsx'
+import Searchbar from './Searchbar.jsx'
+import Recipientbar from './Recipientbar.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <div className="app-container">
+
+            <div className={"sidebar-container"}>
+                <Searchbar></Searchbar>
+                <Sidebar></Sidebar>
+            </div>
+
+            <div className={"chatWindow-container"}>
+                <Recipientbar></Recipientbar>
+                <ChatWindow></ChatWindow>
+            </div>
+
+        </div>
     </>
   )
 }
