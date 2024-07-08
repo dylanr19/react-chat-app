3e1659dcc5edf0ae831c554ccfd2bcd3f1426b34
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-07-08 23:36:10 +0200

    Create self chatbox

--- src/ChatBox.jsx
+++ src/ChatBox.jsx
@@ -15,23 +15,32 @@ function ChatBox({ photo, text, date, isPartner }) {
                     <span className="date">11:02 AM, January, 23rd</span>
                 </div>
             </div>
+
+            <div className="chat-box-self">
+                <div className="message-container">
+                    <span className="text">Hey Jake, Doing good!</span>
+                    <span className="date">11:03 AM, January, 23rd</span>
+                </div>
+            </div>
+
             <div className="chat-box-partner">
                 <div className="photo-container">
                     <img className="photo"
                          src="https://static01.nyt.com/images/2022/06/16/arts/16OLD-MAN1/16OLD-MAN1-mediumSquareAt3X-v3.jpg"/>
                 </div>
                 <div className="message-container">
-                    <span className="text">Have you sent me the documents?</span>
+                    <span className="text">Great! Did you read my document?</span>
                     <span className="date">11:02 AM, January, 23rd</span>
                 </div>
             </div>
 
-            {/*<div className="chat-box-self">*/}
-            {/*    <div className="message-container">*/}
-            {/*        <p className="text">Hey Jake, Doing good!</p>*/}
-            {/*        <p className="date">11:03 AM, January, 23rd</p>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+            <div className="chat-box-self">
+                <div className="message-container">
+                    <span className="text">Yes I did. You passed the test!</span>
+                    <span className="date">11:03 AM, January, 23rd</span>
+                </div>
+            </div>
+
         </>
     )
 }
