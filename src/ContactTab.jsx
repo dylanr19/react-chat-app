dee4c254c853a475c49c19c1467917fee7bbdd58
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-07-11 02:42:44 +0200

    Add sidebar logic
    Add logic for:
    
    1. Setting a tab active/inactive
    2. Resetting Unread Message Count
    3. abstracted upcoming logic related to handling messages
    4. abstracted function call to chat window

--- src/ContactTab.jsx
+++ src/ContactTab.jsx
@@ -1,16 +1,56 @@
 import './App.css'
+import {useEffect, useRef, useState} from 'react'
 
-function ContactTab({ photo, name, lastMessage }) {
+//- ON OPEN PARTNER TAB
+// 	- Change background color of tab to mint blue
+// 	- Change background color of previous tab to white
+// 	- Reset unread message icon to 0
+// 	- Clear chatboxes of current conversation
+// 	- Request chat messages of your conversation with this person from server
+// 	- Load in the chat messages in the chat view
+
+function ContactTab({ photo, name, userId, lastMessage, color, contacts, setContacts }) {
+    const [unreadMessages, setUnreadMessages] = useState(8);
+
+    const incrementUnreadMessages = () => {
+        setUnreadMessages((prevState) => prevState++ );
+    }
+
+    const  switchPartner = () => {
+
+        setContactInactive()
+        setContactActive()
+        resetUnreadMessages()
+        // call function in chat window for performing chatBox related logic and make shared context for handling message api logic
+    }
+
+    const setContactInactive = () => {
+
+        // Only inactive contact tabs have a white color
+        const activeContact = contacts.find( contact => contact.color !== 'white' )
+        activeContact.color = 'white'
+    }
+
+    const setContactActive = () => {
+
+        const activeContact = contacts.find( contact => contact.color === 'white' )
+        activeContact.color = '#f8f9fd'
+    }
+
+    const resetUnreadMessages = () => {
+
+        setUnreadMessages(0)
+    }
 
     return (
         <>
-            <div className="contact-tab">
+            <div className="contact-tab" style={{ background: color }} id={userId} onClick={() => switchPartner(userId)}>
                 <img className="photo" src={photo} alt={"photo of " + {name}}/>
                 <div className="info-container">
-                    <text className="name">{name}</text>
-                    <text className="last-message">{lastMessage}</text>
+                    <div className="name">{name}</div>
+                    <div className="last-message">{lastMessage}</div>
                 </div>
-                <div className="unread-icon">3</div>
+                <div className="unread-icon">{unreadMessages}</div>
             </div>
         </>
     )
