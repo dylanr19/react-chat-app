644bc5283d8fbfc0d840bb09f7a4f5b1fa194ee8
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-07-11 02:37:36 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 import ChatWindow from './ChatWindow.jsx'
 import Sidebar from './Sidebar.jsx'
 import Searchbar from './Searchbar.jsx'
 import Recipientbar from './Recipientbar.jsx'
 import Messagebar from './Messagebar.jsx'
-import ChatBox from './ChatBox.jsx'
 
 function App() {
-  //const [count, setCount] = useState(0)
 
   return (
     <>
