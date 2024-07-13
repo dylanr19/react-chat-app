2be3803c048470329f0baf0d3a7c71c524754964
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-07-13 15:41:30 +0200

    Add logic for sending messages

--- src/MessageContext.jsx
+++ src/MessageContext.jsx
@@ -3,7 +3,7 @@ import {createContext, useEffect, useState} from 'react';
 export const MessageContext = createContext();
 
 export const MessageProvider = ({ children }) => {
-    const messageData = {photoURL: '', senderId: '', date: '', text: '',}
+    const messageData = {photoURL: '', senderId: '', date: '', text: '', delivered: true}
     const [newMessage, setNewMessage] = useState(messageData)
     const [messageHistory, setMessageHistory] = useState([messageData])
 
@@ -35,7 +35,7 @@ export const MessageProvider = ({ children }) => {
     useEffect(() => {
         // Request message list of conversation between this user and its partner from server
         // Set the state with these messages
-        setMessageHistory(requestMessageHistory('xxx', 'user1'))
+        setMessageHistory(requestMessageHistory('xxx', 'xxx'))
     }, [chatPartner]);
 
     const requestMessageHistory = (ownId, partnerId) => {
@@ -44,21 +44,77 @@ export const MessageProvider = ({ children }) => {
                 senderId: 'user1',
                 date: '11:02 AM, January, 23rd',
                 text: 'I am not sure if that can work out.',
+                delivered: true
             },
             {   photoURL: '',
                 senderId: 'ownId',
                 date: '12:02 AM, January, 23rd',
                 text: 'That is not a problem.',
+                delivered: true
             },
             {
                 photoURL: 'https://static01.nyt.com/images/2022/06/16/arts/16OLD-MAN1/16OLD-MAN1-mediumSquareAt3X-v3.jpg',
                 senderId: 'user1',
                 date: '11:02 AM, January, 23rd',
                 text: 'See you next time!',
+                delivered: true
             }]
     }
 
-    const processMessage = (messageData) => {
+    const processOutgoingMessage = (text) => {
+        if (text.trim() === '') {
+            // text is empty
+            return
+        }
+
+        const messageData = {
+            photoURL: '',
+            senderId: 'ownId',
+            date: '1 January 1st 14:05:36',
+            text: text,
+            delivered: true
+        }
+
+        // Render the message in the chat window
+        appendMessageHistory(messageData)
+
+        // Send the message to the server
+        if (sendMessage(messageData) === false) {
+            // show user that message was not delivered
+        }
+
+    }
+
+    const appendMessageHistory = (messageData) => {
+        const msgHistoryCopy = [...messageHistory]
+        msgHistoryCopy.push(messageData)
+        setMessageHistory(msgHistoryCopy)
+    }
+
+    const sendMessage = (messageData) => {
+        console.log('sent message: ' + messageData.text)
+        console.log('waiting for server response...')
+
+        const serverResponse = ''
+
+        if (serverResponse === 'msg received')
+            return true
+
+        if (serverResponse === 'msg denied')
+            return false
+
+        if (serverResponse === 'msg timeout')
+            return false
+    }
+
+    const setMessageFailed = (senderId) => {
+        const copiedMessageHistory = [...messageHistory]
+        const messageData = copiedMessageHistory.find( message => message.userId === senderId)
+        messageData.delivered = false
+        setMessageHistory(copiedMessageHistory)
+    }
+
+    const processIncomingMessage = (messageData) => {
         const partnerListCopy = [...partnerList]
         const partnerData = getPartnerData(messageData.senderId)
         const isSenderNew = partnerData == null
@@ -124,6 +180,7 @@ export const MessageProvider = ({ children }) => {
 
     return (
         <MessageContext.Provider value={{
+            processOutgoingMessage,
             newMessage,
             setNewMessage,
             messageHistory,
--- src/Messagebar.jsx
+++ src/Messagebar.jsx
@@ -1,17 +1,32 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import {useContext, useEffect, useState} from 'react'
+import { MessageContext } from './MessageContext.jsx'
 
 function Messagebar() {
-    //const [count, setCount] = useState(0)
+    const { processOutgoingMessage } = useContext(MessageContext)
+    const [inputValue, setInputValue] = useState('')
+
+    const handleChange = (e) => {
+        setInputValue(e.currentTarget.value)
+    }
+
+    const handleClick = () => {
+        processOutgoingMessage(inputValue)
+        setInputValue('')
+    }
 
     return (
         <>
             <div className="message-bar">
-                <input type="text" className="text-input" placeholder="Type a message here ..." />
-                <button type="button" className="send-button">
+                <input
+                    type="text"
+                    className="text-input"
+                    placeholder="Type a message here ..."
+                    value={inputValue}
+                    onChange={handleChange}
+                />
+                <button type="button" className="send-button" onClick={handleClick}>
                     <i className="bi bi-send-fill"></i>
                 </button>
             </div>
