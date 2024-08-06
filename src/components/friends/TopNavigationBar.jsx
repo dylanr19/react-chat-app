3a10aa461cdc24ecc458f983f029e4197a04b87a
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-08-06 13:13:18 +0200

    Improve topnav styling

--- src/components/friends/TopNavigationBar.jsx
+++ src/components/friends/TopNavigationBar.jsx
@@ -4,11 +4,16 @@ function TopNavigationBar() {
 
     return(
         <>
+            <div className="top-navigation-container">
+                <div className="bi bi-person-raised-hand"></div>
+                <h3 className="title">Friends</h3>
+                <div className="line"></div>
                 <div className="topnav">
-                    <a className="active" href="#news">Friends</a>
+                    <a className="active" href="#news">Accepted</a>
                     <a href="#contact">Pending</a>
                     <a className="add" href="#about">Add Friends</a>
                 </div>
+            </div>
         </>
     )
 }
