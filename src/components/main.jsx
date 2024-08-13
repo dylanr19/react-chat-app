ddcfa41f9171f64022afdbd2330d719a4cf7e00f
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-08-14 00:32:49 +0200

    update file references

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import '../styles/index.css'
-import {ChatProvider} from "../Contexts/chat context/ChatContext.jsx";
-import {LoginProvider} from "../Contexts/login context/LoginContext.jsx";
+import {ChatProvider} from "../Contexts/ChatContext.jsx";
+import {LoginProvider} from "../Contexts/LoginContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
