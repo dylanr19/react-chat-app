5a6cbcce77d1af417fb54a64af535013d3ffd7da
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-10-04 17:59:57 +0200

    Convert Utc time to Localized time

--- src/components/chat/ChatBox.jsx
+++ src/components/chat/ChatBox.jsx
@@ -1,13 +1,31 @@
 import '../../styles/App.css'
-import {useContext} from "react";
+import {useContext, useEffect, useState} from "react";
 import {LoginContext} from "../../Contexts/LoginContext.jsx";
 
 function ChatBox({ photoURL, text, date, senderId }) {
     const { userId: loggedInUserid } = useContext(LoginContext)
+    const [ formattedDate, setFormattedDate ] = useState("")
 
     const isOwnMessage = (senderId) => {
         return senderId === loggedInUserid
     }
+
+    const formatMessageDate = (utcDateString) => {
+        const date = new Date(utcDateString);
+
+        return date.toLocaleString(navigator.language, {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric'
+        });
+    };
+
+    useEffect(() => {
+        setFormattedDate(formatMessageDate(date))
+    }, []);
 
     return (
         <>
@@ -17,7 +35,7 @@ function ChatBox({ photoURL, text, date, senderId }) {
                 <div className={"chat-box-sent"}>
                     <div className="message-container">
                         <span className="text">{text}</span>
-                        <span className="date">{date}</span>
+                        <span className="date">{formattedDate}</span>
                     </div>
                 </div>
                 :
@@ -28,7 +46,7 @@ function ChatBox({ photoURL, text, date, senderId }) {
                     </div>
                     <div className="message-container">
                         <span className="text">{text}</span>
-                        <span className="date">{date}</span>
+                        <span className="date">{formattedDate}</span>
                     </div>
                 </div>
         }
