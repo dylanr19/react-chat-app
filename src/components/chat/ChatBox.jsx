24fe2545227dbf4649e5a576927dffcd1f5c58b7
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-10-04 19:04:17 +0200

    Format time
    - Convert UTC time to Localised time for date in fetched chat messages
    - Only show the time string for messages that were sent today
    - Show full date string for messages that were sent at least a day ago and later

--- src/components/chat/ChatBox.jsx
+++ src/components/chat/ChatBox.jsx
@@ -4,27 +4,45 @@ import {LoginContext} from "../../Contexts/LoginContext.jsx";
 
 function ChatBox({ photoURL, text, date, senderId }) {
     const { userId: loggedInUserid } = useContext(LoginContext)
-    const [ formattedDate, setFormattedDate ] = useState("")
+    const [ localisedDate, setLocalisedDate ] = useState("")
 
     const isOwnMessage = (senderId) => {
         return senderId === loggedInUserid
     }
 
-    const formatMessageDate = (utcDateString) => {
-        const date = new Date(utcDateString);
-
-        return date.toLocaleString(navigator.language, {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric',
-            hour: 'numeric',
-            minute: 'numeric'
-        });
-    };
-
     useEffect(() => {
-        setFormattedDate(formatMessageDate(date))
+        if (date == null){
+            // Chat Message just got sent by local user and doesn't include a date
+            setLocalisedDate(new Date().toLocaleTimeString([], {
+                hour: '2-digit',
+                minute: '2-digit',
+            }))
+
+        }  else {
+            // Chat Message was fetched from server and includes UTC date of when it was sent
+            const messageSentDate = new Date(date)
+            const today = new Date()
+
+            if (today.getDay() === messageSentDate.getDay() &&
+            today.getMonth() === messageSentDate.getMonth() &&
+            today.getFullYear() === messageSentDate.getFullYear()) {
+                // Chat Message was sent today, only display the time
+                setLocalisedDate(messageSentDate.toLocaleTimeString([], {
+                    hour: '2-digit',
+                    minute: '2-digit',
+                }))
+            } else {
+                // Chat Message was sent on another date
+                setLocalisedDate(messageSentDate.toLocaleString([], {
+                    weekday: 'long',
+                    year: 'numeric',
+                    month: 'short',
+                    day: 'numeric',
+                    hour: 'numeric',
+                    minute: 'numeric'
+                }))
+            }
+        }
     }, []);
 
     return (
@@ -35,7 +53,7 @@ function ChatBox({ photoURL, text, date, senderId }) {
                 <div className={"chat-box-sent"}>
                     <div className="message-container">
                         <span className="text">{text}</span>
-                        <span className="date">{formattedDate}</span>
+                        <span className="date">{localisedDate}</span>
                     </div>
                 </div>
                 :
@@ -46,7 +64,7 @@ function ChatBox({ photoURL, text, date, senderId }) {
                     </div>
                     <div className="message-container">
                         <span className="text">{text}</span>
-                        <span className="date">{formattedDate}</span>
+                        <span className="date">{localisedDate}</span>
                     </div>
                 </div>
         }
