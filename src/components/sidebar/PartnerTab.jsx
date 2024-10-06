0dfb743405775f9b7c8c796199f94afa3ef8c986
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-10-06 12:46:21 +0200

    Delete useless code

--- src/components/sidebar/PartnerTab.jsx
+++ src/components/sidebar/PartnerTab.jsx
@@ -1,41 +1,41 @@
 import '../../styles/App.css'
-import {useEffect, useState} from 'react'
+import {useContext} from 'react'
+import {ChatContext} from "../../Contexts/ChatContext.jsx";
 
-function PartnerTab({ partnerData, partnerList, setPartnerList, startNewChat, resetUnreadMessageCount }) {
-
-    const deactivatePreviousTab = (partnerList) => {
-        const activePartner = partnerList.find( partner => partner.isActive === true )
-        if (activePartner == null) {
-            return
-        }
-        activePartner.isActive = false;
-    }
-
-    const activateThisTab = (partnerList) => {
-        const activePartner = partnerList.find( partner => partner.userId === partnerData.userId )
-        activePartner.isActive = true;
-    }
-
-    const switchActiveTab = () => {
-        const copiedPartnerList = [...partnerList]
-        deactivatePreviousTab(copiedPartnerList)
-        activateThisTab(copiedPartnerList)
-        setPartnerList(copiedPartnerList)
-        startNewChat(partnerData)
-    }
+function PartnerTab({ partnerData }) {
+    const {
+        startNewChat,
+        resetUnreadMessageCount,
+        setPartnerActive,
+        setPartnerInactive
+    } = useContext(ChatContext)
 
     const handleTabClick = () => {
         resetUnreadMessageCount(partnerData.userId)
-        switchActiveTab()
+        setPartnerInactive()
+        setPartnerActive(partnerData.userId)
+        startNewChat(partnerData)
     }
 
     return (
         <>
-            <button className="partner-tab" style={{ background: partnerData.isActive ? '#ededed' : 'white' }} id={partnerData.userId} onClick={() => handleTabClick()}>
-                <img className="photo" src={partnerData.photoURL} alt={"photo of " + partnerData.name}/>
+            <button
+                className="partner-tab"
+                style={{ background: partnerData.isActive ? '#ededed' : 'white' }}
+                id={partnerData.userId}
+                onClick={() => handleTabClick()}
+            >
+
+                <img
+                    className="photo"
+                    src={partnerData.photoURL}
+                    alt={"photo of " + partnerData.name}
+                />
+
                 <div className="info-container">
                     <div className="name">{partnerData.name}</div>
                 </div>
+
                 {
                     partnerData.unreadMessageCount === 0
                         ? <div className="placeholder" style={{width: '15px', height: '15px'}}></div>
@@ -44,6 +44,7 @@ function PartnerTab({ partnerData, partnerList, setPartnerList, startNewChat, re
                             <div className="unread-count">{partnerData.unreadMessageCount}</div>
                         </div>
                 }
+                
             </button>
         </>
     )
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,6 +1,6 @@
 import '../../styles/App.css'
 import PartnerTab from './PartnerTab.jsx'
-import {useEffect, useState, useContext} from "react";
+import {useContext} from "react";
 import {ChatContext} from '../../Contexts/ChatContext.jsx'
 
 const placeholder = 'https://static01.nyt.com/images/2022/06/16/arts/16OLD-MAN1/16OLD-MAN1-mediumSquareAt3X-v3.jpg'
@@ -8,7 +8,7 @@ const placeholder2 = 'https://t3.ftcdn.net/jpg/03/85/20/64/360_F_385206426_wllRG
 const placeholder3 = 'https://media.glamour.com/photos/5695c14716d0dc3747ede10d/master/w_1600,c_limit/entertainment-2013-07-patrick-j-adams-main.jpg'
 
 function Sidebar() {
-    const { filteredChatPartners, setFilteredChatPartners, startNewChat, resetUnreadMessageCount } = useContext(ChatContext)
+    const { filteredChatPartners } = useContext(ChatContext)
 
     return (
         <>
@@ -17,10 +17,6 @@ function Sidebar() {
                     filteredChatPartners.map((partner) => (
                     <PartnerTab
                         partnerData={partner}
-                        partnerList={filteredChatPartners}
-                        setPartnerList={setFilteredChatPartners}
-                        startNewChat={startNewChat}
-                        resetUnreadMessageCount={resetUnreadMessageCount}
                         key={partner.userId}
                     />))
                 }
