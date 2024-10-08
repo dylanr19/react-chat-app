0b395354c345c85e5b354a3b96e65efabc921b44
Dylan <67800304+dylanr19@users.noreply.github.com>
2024-10-08 21:14:50 +0200

    Remove old placeholders

--- src/components/sidebar/chat tabs/ChatTabsWindow.jsx
+++ src/components/sidebar/chat tabs/ChatTabsWindow.jsx
@@ -3,10 +3,6 @@ import ChatTab from './ChatTab.jsx'
 import {useContext} from "react";
 import {ChatContext} from '../../../Contexts/ChatContext.jsx'
 
-const placeholder = 'https://static01.nyt.com/images/2022/06/16/arts/16OLD-MAN1/16OLD-MAN1-mediumSquareAt3X-v3.jpg'
-const placeholder2 = 'https://t3.ftcdn.net/jpg/03/85/20/64/360_F_385206426_wllRGLFfXeFHB7x41Jc2Lz5kZjo2PraC.jpg'
-const placeholder3 = 'https://media.glamour.com/photos/5695c14716d0dc3747ede10d/master/w_1600,c_limit/entertainment-2013-07-patrick-j-adams-main.jpg'
-
 function ChatTabsWindow() {
     const { filteredChatTabs } = useContext(ChatContext)
 
